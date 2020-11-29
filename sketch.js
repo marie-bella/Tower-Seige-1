bd11995ccf4c0b55322e3ec2e00ad9b679a242da
marie-bella <71880657+marie-bella@users.noreply.github.com>
2020-11-29 19:04:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var a;
+var circles=[];
+const Bodies = Matter.Bodies;
+
+
+
+
+function setup() {
+  createCanvas(800,800);
+  stroke(255)
+  
+ // camera=new Camera(width/2,height/2,0.5);
+  //camera.on();
+  a=height;
+  circles.push(width/2)
+}
+
+function draw() {
+  
+  background(0);
+  
+  //polygon holder with slings
+
+  polygon = Bodies.circle(50,200,20);
+  World.add(world, polygon);
+
+  slingshot = new slingshot(this.polygon,{x:100, y:200});
+  
+  
+  a=a-1;
+  //camera.zoom=camera.zoom+0.01
+ //camera.position={x:width/2,y:a}
+ 
+  
+  for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+ // camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+ drawSprites();
+}
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+    if(keyIsDown(RIGHT_ARROW))
+    {
+      camera.position.x=camera.position.x+10;
+    }
+  }
+} 
